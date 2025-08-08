99002970238fe3b35d0f430a561007d59f9c3ede
Zheng-Chen-WH <zhengchenwh@gmail.com>
2025-08-08 17:37:14 +0800

    parameters modified

--- SACfD-SD/config.py
+++ SACfD-SD/config.py
@@ -59,7 +59,7 @@ EVAL_FREQ = 1 #训练过程中每多少个epoch之后进行测试
 BASELINE_UPDATE = 500 # 计算rl与il混合权重时的update次数间隔
 UPDATE_THRESHOLD = 0.8  # 基线更新的阈值因子,新的参考值小于gamma*参考值时才更新
 K_FINAL = 2.5 # 控制从Q网络表现到强化学习权重的映射函数，值越小对rl权重越大,2.5的时候似乎比较合适
-K_RL_THRESHOLD = UPDATE_THRESHOLD ** 3 # 控制td和dis两个参数相比初期的最大值，避免rl训练平稳期比重反而下降的问题
+K_RL_THRESHOLD = UPDATE_THRESHOLD ** 4 # 控制td和dis两个参数相比初期的最大值，避免rl训练平稳期比重反而下降的问题
 
 
 # 穿门任务专用参数
